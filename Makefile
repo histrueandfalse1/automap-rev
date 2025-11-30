73184e995d30f9fa224da0e548fc0d294ebd2f55
histrueandfalse1 <sjmcci11@outlook.com>
2025-11-30 15:03:32 +1100

    defined project structure

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 build:
 	@echo "Building project..."
-	
+	mkdir -p bin 
+	clang++ src/core/main.cpp -o bin/main -framework CoreMIDI -framework CoreFoundation
+
+clean:
+	rm -Rf bin
--- src/interface/window.py
+++ src/interface/window.py
@@ -1,1 +1,15 @@
 from PySide6.QtWidgets import QMainWindow, QApplication
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Novation-Automap-Legacy-Bridge")
+        self.setGeometry(100,100,500,500)
+
+app = QApplication([])
+
+window = Window()
+window.show()
+
+app.exec()
+
